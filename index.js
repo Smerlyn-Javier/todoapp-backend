f95e80d9551890c2814cbf018232bb2ce165cd40
Smerlyn-Javier <smerlyn88@gmail.com>
2019-07-26 15:16:51 -0400

    middleware and routes cofigured

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const { PORT } = require("./config");
+const { TodoRoutes } = require("./routes");
+
+app.use(express.json());
+
+app.use("/api/todo", TodoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Application running on port ${PORT}`);
